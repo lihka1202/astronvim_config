888ccd07917c2fdfcccad8c73bd2c684820d62d2
Akhil Narayanachary <akhil.narayanachary@Akhils-Air.local>
2024-01-03 10:42:15 +0800

    debug : Sorted merging issues locally
    Tested out the tsserver, not installed as the file type not present for the linter.

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -8,10 +8,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
-<<<<<<< HEAD
-=======
         "tsserver",
->>>>>>> baa82a0e49bd432c810878c80d4f895ca01ba23b
       })
     end,
   },
