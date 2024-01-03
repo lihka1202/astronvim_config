baa82a0e49bd432c810878c80d4f895ca01ba23b
Akhil MN <mnakhilwork@gmail.com>
2024-01-03 08:09:35 +0800

    chore : added TypeScript LSP support
    Add `tsserver` which gives typescript LSP Support, test using eslinter for chrome extension

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "tsserver",
       })
     end,
   },
@@ -18,18 +19,18 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "prettier",
+        "stylua",
       })
     end,
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
+    -- overrides require("mason-nvim-dap").setup(...)
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "python",
       })
     end,
   },
