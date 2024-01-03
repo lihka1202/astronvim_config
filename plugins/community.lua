1feeed2571b773cea75539abec97502a1dfb640e
Akhil Narayanachary <akhil.narayanachary@Akhils-Air.local>
2024-01-03 12:10:26 +0800

    chore : colorschema has been set up, Astrodark stays
    I think astrodark generally has better syntax highlighting in comparison to the others.
    Just keep this in for the time being and see how this goes.

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,7 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  -- { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.typescript" },
 }
