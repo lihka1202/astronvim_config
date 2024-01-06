69787add6b51d462a5a9111a41ca1577f5fa3312
lihka1202 <mnakhilwork@gmail.com>
2024-01-06 09:38:53 +0800

    feat : neotest support added to the Lua condfig

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,24 @@
 return {
+  {
+    "nvim-neotest/neotest",
+    requires = {
+      -- ...
+      "thenbe/neotest-playwright",
+    },
+    config = function()
+      require("neotest").setup {
+        -- ...
+        adapters = {
+          require("neotest-playwright").adapter {
+            options = {
+              persist_project_selection = true,
+              enable_dynamic_test_discovery = true,
+            },
+          },
+        },
+      }
+    end,
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
